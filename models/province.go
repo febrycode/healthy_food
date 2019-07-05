14a2ee18e3f15dcb36a305bb3ef29299eb3ffe0a
Febryan Setiawan <setiawanfebryan@gmail.com>
2019-07-06 06:32:02 +0700

    bugfix

--- models/province.go
+++ models/province.go
@@ -4,8 +4,8 @@ import "time"
 
 // Province represent province table
 type Province struct {
-	ID        int64     `json:"id" db:"id"`
-	Name      string    `json:"name" db:"name"`
-	CreatedAt time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	ID        int64     `json:"id"            db:"id"`
+	Name      string    `json:"name"          db:"name"`
+	CreatedAt time.Time `json:"created_at"    db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"    db:"updated_at"`
 }
